3324a80967b27c0751897827b146d9346d2aeb2a
Mark Wilkins <contactme@markwilkins.co>
2020-07-25 00:46:40 -0400

    initial commit

---
+++ js.js
@@ -0,0 +1,65 @@
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+
+const addPart = () => {
+  const part_list = document.getElementById("part-list");
+  const num = part_list.children.length;
+  part_list.insertAdjacentHTML(
+    "beforeend",
+    `
+  <li class="flex">
+    <p>${num + 1}</p>
+    <p class="third-width content_editable" contentEditable="true"></p>
+    <p class="third-width content_editable" contentEditable="true"></p>
+    <p class="third-width content_editable" contentEditable="true"></p>
+  </li>
+  `
+  );
+};
+
+const removePart = () => {
+  const part_list = document.getElementById("part-list");
+  const last_child = part_list.children[part_list.children.length - 1];
+  part_list.removeChild(last_child);
+};
+
+const download = () => {
+  const quality = 2;
+  const filename = "Invoice.pdf";
+  document.body.classList.add("printing");
+
+  requestAnimationFrame(() => {
+    html2canvas(document.querySelector("body"), {
+      scale: quality,
+    })
+      .then((canvas) => {
+        let pdf = new jsPDF("p", "mm", "a4");
+        pdf.addImage(canvas.toDataURL("image/png"), "PNG", 0, 0, 211, 298);
+        pdf.save(filename);
+        document.body.classList.remove("printing");
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("something broke, sorry!");
+      });
+  });
+};
+
+const main = () => {
+  const addBttn = document.getElementById("add_part");
+  const subBttn = document.getElementById("sub_part");
+  const dwnldBttn = document.getElementById("download");
+
+  addBttn.addEventListener("click", addPart);
+  subBttn.addEventListener("click", removePart);
+  dwnldBttn.addEventListener("click", download);
+};
+
+if (
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  main();
+} else {
+  document.addEventListener("DOMContentLoaded", main);
+}
